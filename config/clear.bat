d981743e41c27cdce5d5d50bad4333881cfbd23d
fullstackdev6001 <vs8990r@gmail.com>
2020-12-13 07:50:41 +0900

    deleted wrong files

--- config/clear.bat
+++
@@ -1,7 +0,0 @@
-php artisan config:cache
-php artisan cache:clear
-php artisan config:clear
-php artisan view:clear
-php artisan route:cache
-composer clearcache
-composer dump-autoload
--- config/resources/views/user/show.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.admin', ['menu_name' => 'user', 'sub_menu_name' => ''])
-
-@section('content')
-@endsection
